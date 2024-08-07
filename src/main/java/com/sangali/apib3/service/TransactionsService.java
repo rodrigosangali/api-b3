270a6a1e16b7b760fb511bcb5de2cb594413b0b8
Rodrigo <rodrigo.sangali@gmail.com>
2024-08-06 22:08:39 -0300

    incluido log e criado tabela novamente

--- src/main/java/com/sangali/apib3/service/TransactionsService.java
+++ src/main/java/com/sangali/apib3/service/TransactionsService.java
@@ -4,6 +4,7 @@ import com.sangali.apib3.entity.Transactions;
 import com.sangali.apib3.model.LinhaExcel;
 import com.sangali.apib3.repository.TransactionsRepository;
 import com.sangali.apib3.utils.SHA1HasGenerator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,7 @@ import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
 @Service
+@Slf4j
 public class TransactionsService {
     @Autowired
     private TransactionsRepository transactionsRepository;
@@ -22,6 +24,8 @@ public class TransactionsService {
         realExcelFileService = new RealExcelFileService();
         // Le o execel na pasta
         List<LinhaExcel> linhasExcel = realExcelFileService.lerExcelTransactions();
+
+        log.info("Quantidade de linhas excel: " + linhasExcel.size());
 
         // Verifica se há duplicidade pelo hash
         Iterator<LinhaExcel> iterator = linhasExcel.iterator();
@@ -45,7 +49,7 @@ public class TransactionsService {
         linhasExcel.forEach( linha -> {
             transactionsRepository.save(new Transactions(linha));
         });
-
+        log.info("Quantidade de linhas inseridas: " + linhasExcel.size());
         System.out.println("Quantidade de linhas inseridas: " + linhasExcel.size());
     }
 
